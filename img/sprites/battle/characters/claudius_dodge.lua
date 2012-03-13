7be42e194cb47b832a30fec70665645ad834bb6b
Yohann Ferreira <yohann_dot_ferreira_at_orange_dot_efer>
2012-03-13 18:04:27 +0100

    Updated Claudius's dodge animation by using the idle south map animation.

--- img/sprites/battle/characters/claudius_dodge.lua
+++ img/sprites/battle/characters/claudius_dodge.lua
@@ -8,10 +8,10 @@ animation = {
 	-- The number of rows and columns of images, will be used to compute
 	-- the images width and height, and also the frames number (row x col)
 	rows = 1,
-	columns = 1,
+	columns = 4,
 	-- set the image dimensions on battles (in pixels)
 	frame_width = 128.0,
 	frame_height = 128.0,
 	-- The frames duration in milliseconds
-	frames_duration = { 600 }
+	frames_duration = { 150, 150, 150, 150 }
 }
