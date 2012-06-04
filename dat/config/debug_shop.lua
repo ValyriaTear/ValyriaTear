4075e0ba27a8496e151dc61bb47292c44a558eaa
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2012-06-04 18:38:24 +0200

    Readded the character sprite display on equipment view.
    I chose to use a battle sprite copy and to animate it when the item
    is equipable.
    
    Changing from one view to the equipment view can be slow on certain
    configs. Since the bug was already there without that change,
    I'll investigate this in another commit.

--- dat/config/debug_shop.lua
+++ dat/config/debug_shop.lua
@@ -1,9 +1,11 @@
 -- Debug shop script
 function BootShopTest()
 	print("BootShopTest");
-    -- The shop window might still be crash prone
 
 	GlobalManager:AddCharacter(BRONANN);
+	GlobalManager:AddCharacter(KALYA);
+	GlobalManager:AddCharacter(SYLVE);
+	GlobalManager:AddCharacter(THANIS);
 
 	-- Initial character money
 	GlobalManager:AddDrunes(1842);
@@ -15,22 +17,23 @@ function BootShopTest()
 
 	local shop = hoa_shop.ShopMode();
 
-	--shop:AddObject(1, 12);
-	--shop:AddObject(1001, 5);
-	--shop:AddObject(10001, 2);
+	shop:AddObject(1, 12);
+	shop:AddObject(1001, 5);
+	shop:AddObject(10001, 2);
 	shop:AddObject(10002, 4);
 	shop:AddObject(10003, 12);
+	shop:AddObject(11001, 5);
 	shop:AddObject(20002, 1);
-	--shop:AddObject(20003, 2);
-	--shop:AddObject(30001, 3);
-	--shop:AddObject(30002, 10);
-	--shop:AddObject(30003, 11);
-	--shop:AddObject(40001, 2);
-	--shop:AddObject(40002, 1);
-	--shop:AddObject(40003, 1);
-	--shop:AddObject(40004, 1);
-	--shop:AddObject(50001, 1);
-	--shop:AddObject(50002, 1);
+	shop:AddObject(20003, 2);
+	shop:AddObject(30001, 3);
+	shop:AddObject(30002, 10);
+	shop:AddObject(30003, 11);
+	shop:AddObject(40001, 2);
+	shop:AddObject(40002, 1);
+	shop:AddObject(40003, 1);
+	shop:AddObject(40004, 1);
+	shop:AddObject(50001, 1);
+	shop:AddObject(50002, 1);
 
 	ModeManager:Push(shop, false, false);
 
