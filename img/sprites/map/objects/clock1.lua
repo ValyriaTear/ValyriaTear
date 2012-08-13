139e3b05b580c7910aa54c5418deecd106c5c62b
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2012-08-13 21:05:32 +0200

    Finished a first version of Lilly's house.
    - Fixed some collisions
    - Added some objects.
    - Optimized a bit the way the clock is loaded.

--- img/sprites/map/objects/clock1.lua
+++ img/sprites/map/objects/clock1.lua
@@ -4,23 +4,23 @@
 animation = {
 
 	-- The file to load the frames from
-	image_filename = "img/sprites/map/objects/clock1.png",
+	image_filename = "img/tilesets/building_interior_objects_01.png",
 	-- The number of rows and columns of images, will be used to compute
 	-- the images width and height, and also the frames number (row x col)
-	rows = 1,
-	columns = 8,
+	rows = 16,
+	columns = 16,
 	-- set the image dimensions on the map (in pixels)
 	frame_width = 32,
 	frame_height = 32,
 	-- The frames duration in milliseconds
     frames = {
-        [0] = { id = 0, duration = 150 },
-        [1] = { id = 1, duration = 150 },
-        [2] = { id = 2, duration = 300 },
-        [3] = { id = 3, duration = 150 },
-        [4] = { id = 4, duration = 150 },
-        [5] = { id = 5, duration = 150 },
-        [6] = { id = 6, duration = 300 },
-        [7] = { id = 7, duration = 150 }
+        [0] = { id = 240, duration = 150 },
+        [1] = { id = 241, duration = 150 },
+        [2] = { id = 242, duration = 300 },
+        [3] = { id = 243, duration = 150 },
+        [4] = { id = 244, duration = 150 },
+        [5] = { id = 245, duration = 150 },
+        [6] = { id = 246, duration = 300 },
+        [7] = { id = 247, duration = 150 }
     }
 }
