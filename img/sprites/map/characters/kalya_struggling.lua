c314e14c8ca1237ef1a1545ef48fa5a033495959
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2013-03-14 18:54:12 +0100

    Tried to improve a bit Kalya's struggling custom animation.

--- img/sprites/map/characters/kalya_struggling.lua
+++ img/sprites/map/characters/kalya_struggling.lua
@@ -3,15 +3,19 @@
 
 animation = {
 
-	-- The file to load the frames from
-	image_filename = "img/sprites/map/characters/kalya_struggling.png",
-	-- The number of rows and columns of images, will be used to compute
-	-- the images width and height, and also the frames number (row x col)
-	rows = 1,
-	columns = 2,
-	-- The frames duration in milliseconds
+    -- The file to load the frames from
+    image_filename = "img/sprites/map/characters/kalya_struggling.png",
+    -- The number of rows and columns of images, will be used to compute
+    -- the images width and height, and also the frames number (row x col)
+    rows = 1,
+    columns = 4,
+    -- The frames duration in milliseconds
     frames = {
-        [0] = { id = 0, duration = 900 },
-        [1] = { id = 1, duration = 900 }
+        [0] = { id = 0, duration = 150 },
+        [1] = { id = 3, duration = 150 },
+        [2] = { id = 0, duration = 150 },
+        [3] = { id = 3, duration = 150 },
+        [4] = { id = 1, duration = 800 },
+        [5] = { id = 2, duration = 75 }
     }
 }
