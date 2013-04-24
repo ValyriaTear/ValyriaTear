f89d934cf41dc71d3ecee23d1286257071c624f1
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2013-04-24 09:06:58 +0200

    Fixed the weapon image used for wood sword doge animation.

--- img/sprites/battle/characters/bronann/weapons/wood_bronze/bronann_dodge.lua
+++ img/sprites/battle/characters/bronann/weapons/wood_bronze/bronann_dodge.lua
@@ -4,7 +4,7 @@
 animation = {
 
 	-- The file to load the frames from
-	image_filename = "img/sprites/battle/characters/bronann/weapons/steel/bronann_dodge.png",
+	image_filename = "img/sprites/battle/characters/bronann/weapons/wood_bronze/bronann_dodge.png",
 	-- The number of rows and columns of images, will be used to compute
 	-- the images width and height, and also the frames number (row x col)
 	rows = 1,
