a36401d56408bc089e3244573101985eac878700
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2013-05-13 22:36:35 +0000

    Used offsets to fix Banesore attack point.

--- dat/maps/layna_village/layna_village_riverbank_at_night_script.lua
+++ dat/maps/layna_village/layna_village_riverbank_at_night_script.lua
@@ -703,7 +703,7 @@ function _CreateEvents()
     event = vt_map.BattleEncounterEvent("Battle with Banesore");
     event:SetMusic("mus/the_recon_mission.ogg");
     event:SetBackground("img/backdrops/battle/mountain_village_single_house.png");
-    event:AddEnemy(10, 700, 600);
+    event:AddEnemy(10, 640, 575);
     event:AddScript("dat/maps/layna_village/battle_with_banesore/battle_with_banesore_script.lua");
     event:AddScript("dat/maps/common/at_night.lua");
 
