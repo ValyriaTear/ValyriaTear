a71d48215bc5cf5fbe8640795544a49eb4709638
Yohann Ferreira <yohann_ferreira_at_orange_fr_>
2013-05-16 23:36:36 +0000

    Started adding the spirits data. Not loaded yet.

--- dat/objects/spirits.lua
+++ dat/objects/spirits.lua
@@ -11,49 +11,107 @@ end
 -- IDs 60,001 - 70,000 are reserved for spirits
 -- -----------------------------------------------------------------------------
 
-spirits[60001] = {
+local ARDORIS  = 60001;
+local UNDINE   = 60002;
+local SYLPHID  = 60003;
+local DRYAD    = 60004;
+local FAIRY    = 60005;
+local DEMONA   = 60006;
+local OBLIVION = 60007;
+
+spirits[ARDORIS] = {
     name = vt_system.Translate("Ardoris"),
     description = vt_system.Translate("An enchanting child of Fire."),
     icon = "img/icons/items/ardoris.png",
-    standard_price = 4500
+    standard_price = 4500,
+    effects = {
+        [0] = { element = GLOBAL_ELEMENTAL_FIRE, intensity = GLOBAL_INTENSITY_POS_LESSER },
+        [1] = { element = GLOBAL_ELEMENTAL_WATER, intensity = GLOBAL_INTENSITY_NEG_LESSER }
+    },
+    combinations = {
+        -- [Combined with] = skill id obtained
+        attack = {
+            [ARDORIS]  = 10100, -- Fire burst
+            [UNDINE]   = 10162, -- ? (Antipode)
+            [SYLPHID]  = 10104, -- Fire Breath
+            [DRYAD]    = 10102, -- Lava
+            [FAIRY]    = 10106, -- Holy Fire
+            [DEMONA]   = 10108, -- Petrify
+            [OBLIVION] = 10163  -- Counter
+        },
+        defence = {
+            [ARDORIS]  = 10101, -- Fire circle
+            [UNDINE]   = 10103, -- Fog
+            [SYLPHID]  = 10105, -- Focus
+            [DRYAD]    = 10001, -- Shield
+            [FAIRY]    = 10107, -- Life
+            [DEMONA]   = 10109, -- Rage
+            [OBLIVION] = 10164  -- Phoenix Call
+        }
+    }
 }
 
-spirits[60002] = {
+spirits[UNDINE] = {
     name = vt_system.Translate("Undine"),
     description = vt_system.Translate("A mesmerizing spirit of living water."),
     icon = "img/icons/items/undine.png",
-    standard_price = 4500
+    standard_price = 4500,
+    effects = {
+        [0] = { element = GLOBAL_ELEMENTAL_WATER, intensity = GLOBAL_INTENSITY_POS_LESSER },
+        [1] = { element = GLOBAL_ELEMENTAL_FIRE, intensity = GLOBAL_INTENSITY_NEG_LESSER }
+    },
+    combinations = {
+        -- [Combined with] = skill id obtained
+        attack = {
+            [ARDORIS]  = 10162, -- ? (Antipode)
+            [UNDINE]   = 10120, -- Wave
+            [SYLPHID]  = 10122, -- Whirlwind
+            [DRYAD]    = 10112, -- Entangle
+            [FAIRY]    = 10124, -- Blindness
+            [DEMONA]   = 10126, -- Poison
+            [OBLIVION] = 10128  -- Tsunami
+        },
+        defence = {
+            [ARDORIS]  = 10103, -- Fog
+            [UNDINE]   = 10121, -- Water circle
+            [SYLPHID]  = 10123, -- Slow
+            [DRYAD]    = 10113, -- Magical Armor
+            [FAIRY]    = 10125, -- Heal
+            [DEMONA]   = 10127, -- Stun
+            [OBLIVION] = 10129  -- Heal Rain
+        }
+    }
 }
 
-spirits[60003] = {
+spirits[SYLPHID] = {
     name = vt_system.Translate("Sylphid"),
-    description = vt_system.Translate("A very rare spirit, usually neither tamable nor catchable."),
+    description = vt_system.Translate("A very rare spirit of Wind, usually neither tamable nor catchable."),
     icon = "img/icons/items/sylphid.png",
     standard_price = 4500
 }
 
-spirits[60004] = {
+spirits[DRYAD] = {
     name = vt_system.Translate("Dryad"),
     description = vt_system.Translate("A fragile spirit, child of Mother Nature."),
     icon = "img/icons/items/dryad.png",
     standard_price = 4500
 }
 
-spirits[60005] = {
+spirits[FAIRY] = {
     name = vt_system.Translate("Fairy"),
     description = vt_system.Translate("Blinding as the sun, she brings life even where Death belongs."),
     icon = "img/icons/items/fairy.png",
     standard_price = 5500
 }
 
-spirits[60006] = {
+spirits[DEMONA] = {
     name = vt_system.Translate("Demona"),
     description = vt_system.Translate("Spirit of the Dark. It can't be possessed: it only chose to be possessed, waiting for its hour..."),
     icon = "img/icons/items/demona.png",
     standard_price = 5500
 }
 
-spirits[60007] = {
+spirits[OBLIVION] = {
     name = vt_system.Translate("Oblivion"),
     description = vt_system.Translate("Touched neither by the time nor space, this spirit is dwelling in pure energy."),
     icon = "img/icons/items/oblivion.png",
