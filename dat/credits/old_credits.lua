89b6f4c0eefa5cbcde13944d674c265aea04791c
Yohann Ferreira <yohann.ferreira@orange.fr>
2013-08-08 17:04:57 +0200

    Updated Allacrostian credits while I see it.

--- dat/credits/old_credits.lua
+++ dat/credits/old_credits.lua
@@ -1,31 +1,28 @@
 ------------------------------------------------------------------------------[[
 -- Filename: credits.lua
 --
--- Description: Contains the credits as they are seen on the screen. This file
--- is updated for every release to reflect any new additions or to
--- re-arrange the ordering.
+-- Description: Contains the credits that will be shown when talking about
+-- Allacrost at the game ending.
 ------------------------------------------------------------------------------]]
 
-credits_text =
-	"Valyria Tear\n" ..
-	"Development: Bertram" ..
-	"\n\n\n" ..
-	"Until 2011: Hero of Allacrost Development Team\n" ..
+allacrost_credits_text =
+	"Until 2011: Valyria Tear was Hero of Allacrost" ..
 	"\n\n\n" ..
 
 -- Special credit levels
+        "Allacrost Development Team\n\n" ..
 	"Founder and Lead Designer\n\n" ..
 	"Tyler Olsen (Roots)\n" ..
 	"\n\n" ..
 
-	"Project Leads/Administrators\n\n" ..
+	"Allacrost Project Leads/Administrators\n\n" ..
 	"Tyler Olsen (Roots)\n" ..
 	"Jacob Rudolph (rujasu)\n" ..
 	"Philip Vorsilak (gorzuate)\n" ..
 	"\n\n" ..
 
 -- Main credit level
-	"Programming Team\n\n" ..
+	"Allacrost rogramming Team\n\n" ..
 	"Andy Gardner (ChopperDave) - battle code, menu code\n" ..
 	"Baris Soner Usakli (Black Knight) - game editor\n" ..
 	"Daniel Steuernol (Steu) - game engine\n" ..
@@ -39,7 +36,7 @@ credits_text =
 	"Wouter Lindenhof (ElonNarai) - game editor\n" ..
 	"\n\n" ..
 
-	"Artwork Team\n\n" ..
+	"Allacrost Artwork Team\n\n" ..
 	"Brett Steele (Safir-Kreuz) - map sprites, battle sprites, character portraits, location graphics\n" ..
 	"Joe Raucci (Sylon) - battle sprites, old GUI artwork\n" ..
 	"Josiah Tobin (Josiah Tobin) - map tiles\n" ..
@@ -47,7 +44,7 @@ credits_text =
 	"Richard Kettering (Jetryl) - map tiles, map sprites, inventory icons, artwork coordination\n" ..
 	"\n\n" ..
 
-	"Music and Sound Team\n\n" ..
+	"Allacrost Music and Sound Team\n\n" ..
 	"Jamie Bremaneson (Jam) - sound composer\n" ..
 	"Joe Rouse (Loodwig) - soundtrack composer\n" ..
 	"Ryan Reilly (Rain) - music and sound lead, soundtrack composer\n" ..
@@ -55,47 +52,12 @@ credits_text =
 	"Zhe Zhou (shizeet) - sound composer\n" ..
 	"\n\n" ..
 
-	"Translation Team\n\n" ..
-	"Marcos Avila Isidoro (marcavis) - Portuguese translation\n" ..
-	"Yohann Ferreira (Bertram) - French translation\n" ..
-	"Andreas Pawlak (nemesis) - German translation\n" ..
-	"\n\n" ..
-
-	"Map Designers\n\n" ..
-	"Richard Kettering (Jetryl)\n" ..
-	"Tyler Olsen (Roots)\n" ..
-	"Jacob Rudolph (rujasu)\n" ..
-	"\n\n" ..
-
-	"Team Managers\n\n" ..
-	"Emre Motan (emrebfg)\n" ..
-	"Matt Gallivan (Gallivan)\n" ..
-	"\n\n" ..
-
-	"Online Services\n\n" ..
-	"Daniel Steuernol (Steu) - wiki support\n" ..
-	"Emre Motan (emrebfg) - website content, forum administration\n" ..
-	"Heikki Naski (raging_hog) - content management system\n" ..
-	"Matt Gallivan (Gallivan) - administration\n" ..
-	"Matthew James (nunvuru) - website design\n" ..
-	"\n\n" ..
-
-	"Story\n\n" ..
-	"Brian Aloisi (Brian Aloisi) - writer, editor\n" ..
-	"Tim Hargreaves (Balthazar) - editor\n" ..
-	"Tyler Olsen (Roots) - author\n" ..
-	"\n\n" ..
-
-	"Packaging\n\n" ..
-	"Dmitry Marakasov (AMDmi3) - FreeBSD distribution\n" ..
-	"Jorda Polo (ettin) - Debian distribution\n" ..
-	"Jacob Rudolph (rujasu) - Windows distribution\n" ..
-	"Philip Vorsilak (gorzuate) - OS X distribution\n" ..
-	"Tyler Olsen (Roots) - Source distribution\n" ..
+	"Allacrost Map Designers\n\n" ..
+	"Richard Kettering (Jetryl) (For the village center in Valyria Tear)\n" ..
 	"\n\n" ..
 
 -- Additional credit level
-	"Additional Programming\n\n" ..
+	"Allacrost Additional Programming\n\n" ..
 	"Aaron Smith (Etherstar) - audio engine\n" ..
 	"Alastair Lynn (prophile) - various code, Mac OS X maintenance\n" ..
 	"Brandon Barnes (Winter Knight) - game engine, game modes, Windows build scripts\n" ..
@@ -111,7 +73,7 @@ credits_text =
 	"Andreas Pawlak (nemesis) - various code\n" ..
 	"\n\n" ..
 
-	"Additional Artwork\n\n" ..
+	"Allacrost Additional Artwork\n\n" ..
 	"Adam Black (indigoshift) - concept art\n" ..
 	"Blake Heatly (KaelisEbonrai) - inventory icons\n" ..
 	"Chris Hopp (fydo) - map sprite animations\n" ..
@@ -135,28 +97,19 @@ credits_text =
 	"(Psiweapon) - map tiles, battle sprites\n" ..
 	"\n\n" ..
 
-	"Additional Music and Sound\n\n" ..
+	"Allacrost Additional Music and Sound\n\n" ..
 	"Jean Malary (hamiko) - sound mixer\n" ..
 	"Matt Dexter (Star Pilot) - soundtrack composer\n" ..
 	"\n\n" ..
 
-	"Additional Internet Services\n\n" ..
-	"Felix Kastner (Biohazard) - previous website\n" ..
-	"Tim Hargreaves (Balthazar) - previous website, forum administration\n" ..
-	"\n\n" ..
-
-	"Additional Translation\n\n" ..
-	"Mikko Hanninen (Burnsaber) - Finnish prologue\n" ..
-	"\n\n" ..
-
 -- Extra credit level
-	"Extra Thanks\n\n" ..
+	"Allacrost Extra Thanks\n\n" ..
 	"(Melchior)\n" ..
 	"(Egan1)\n" ..
 	"\n\n" ..
 
 -- Special credit level
-	"Special Thanks\n\n" ..
+	"Allacrost Special Thanks\n\n" ..
 	"Daniel Cook (Danc) - Map Tiles\n" ..
 	"The Battle for Wesnoth Development Team - Inventory Icons\n" ..
 	"(neoriceisgood) - map monster sprites\n"
