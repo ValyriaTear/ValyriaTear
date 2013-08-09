1acc04e68a222d34758608dfbf2c0e78a90c3d13
Yohann Ferreira <yohann.ferreira@orange.fr>
2013-08-09 21:41:46 +0200

    Added a beautiful yet discret ripples effect in battle when fighting under the rain.

--- dat/maps/mt_elbrus/mt_elbrus_script.lua
+++ dat/maps/mt_elbrus/mt_elbrus_script.lua
@@ -6,6 +6,8 @@ setfenv(1, ns);
 local Battle = {};
 local Script = {};
 
+local ripples_id = -1;
+
 function Initialize(battle_instance)
     Battle = battle_instance;
     Script = Battle:GetScriptSupervisor();
@@ -13,6 +15,8 @@ function Initialize(battle_instance)
     -- Make the rain starts if needed
     if (GlobalManager:GetEventValue("story", "mt_elbrus_weather_level") > 0) then
         Battle:TriggerBattleParticleEffect("dat/effects/particles/rain.lua", 512.0, 768.0);
+
+        ripples_id = Script:AddAnimation("img/effects/ripples.lua", 256.0, 153.6); -- 256.0 * 0.6
     end
 
 end
@@ -25,6 +29,16 @@ end
 --function Update()
 
 --end
+
+function DrawBackground()
+    if (ripples_id > -1) then
+        Script:DrawAnimation(ripples_id, 235.0, 340.0);
+        Script:DrawAnimation(ripples_id, 235.0, 596.0);
+        Script:DrawAnimation(ripples_id, 491.0, 340.0);
+        Script:DrawAnimation(ripples_id, 491.0, 596.0);
+    end
+
+end
 
 --function DrawPostEffects()
 
---
+++ img/effects/ripples.lua
@@ -0,0 +1,48 @@
+-- Animation file descriptor
+-- This file will describe the frames used to load an animation
+
+animation = {
+
+    -- The file to load the frames from
+    image_filename = "img/effects/ripples.png",
+    -- The number of rows and columns of images, will be used to compute
+    -- the images width and height, and also the frames number (row x col)
+    rows = 6,
+    columns = 5,
+    -- set the image dimensions on battles (in pixels)
+    frame_width = 128.0,
+    frame_height = 128.0,
+    -- The frames duration in milliseconds
+    frames = {
+        [0] = { id = 0, duration = 100 },
+        [1] = { id = 1, duration = 100 },
+        [2] = { id = 2, duration = 100 },
+        [3] = { id = 3, duration = 100 },
+        [4] = { id = 4, duration = 100 },
+        [5] = { id = 5, duration = 100 },
+        [6] = { id = 6, duration = 100 },
+        [7] = { id = 7, duration = 100 },
+        [8] = { id = 8, duration = 100 },
+        [9] = { id = 9, duration = 100 },
+        [10] = { id = 10, duration = 100 },
+        [11] = { id = 11, duration = 100 },
+        [12] = { id = 12, duration = 100 },
+        [13] = { id = 13, duration = 100 },
+        [14] = { id = 14, duration = 100 },
+        [15] = { id = 15, duration = 100 },
+        [16] = { id = 16, duration = 100 },
+        [17] = { id = 17, duration = 100 },
+        [18] = { id = 18, duration = 100 },
+        [19] = { id = 19, duration = 100 },
+        [20] = { id = 20, duration = 100 },
+        [21] = { id = 21, duration = 100 },
+        [22] = { id = 22, duration = 100 },
+        [23] = { id = 23, duration = 100 },
+        [24] = { id = 24, duration = 100 },
+        [25] = { id = 25, duration = 100 },
+        [26] = { id = 26, duration = 100 },
+        [27] = { id = 27, duration = 100 },
+        [28] = { id = 28, duration = 100 },
+        [29] = { id = 29, duration = 100 },
+    }
+}
