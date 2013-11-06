c51eb0ad7d74bafdffa410f050647ac14ab463c3
Yohann Ferreira <yohann.ferreira@orange.fr>
2013-11-06 22:33:24 +0100

    Finally found a rather simple way to make the character move speed be more or less the same whatever the angle between the character and the enemy is.
    I've only fixed Bronann's normal attack script for now and fix the others
    in another commit.

--- dat/battles/characters_animations/bronann_attack.lua
+++ dat/battles/characters_animations/bronann_attack.lua
@@ -57,30 +57,40 @@ function Initialize(_character, _target, _skill)
 
     damage_triggered = false;
 
-    distance_moved_x = SystemManager:GetUpdateTime() / vt_map.MapMode.NORMAL_SPEED * 150.0;
+    distance_moved_x = SystemManager:GetUpdateTime() / vt_map.MapMode.NORMAL_SPEED * 170.0;
     local y_diff = character_pos_y - enemy_pos_y;
     if (y_diff == 0.0) then
         a_coeff = 0.0;
         distance_moved_y = 0.0;
     else
-        a_coeff = (enemy_pos_x - character_pos_x) / (character_pos_y - enemy_pos_y);
+        a_coeff = (character_pos_y - enemy_pos_y) / (enemy_pos_x - character_pos_x);
         if (a_coeff < 0) then a_coeff = -a_coeff; end
-        distance_moved_y = (1/a_coeff) * distance_moved_x;
+        distance_moved_y = a_coeff * distance_moved_x;
     end
 
     --print("distance x: ", enemy_pos_x - character_pos_x)
     --print("distance y: ", character_pos_y - enemy_pos_y)
-    --print (distance_moved_x, 1/a_coeff, distance_moved_y);
+    --print (distance_moved_x, a_coeff, distance_moved_y);
 end
 
 
 function Update()
     -- The update time can vary, so update the distance on each update as well.
-    distance_moved_x = SystemManager:GetUpdateTime() / vt_map.MapMode.NORMAL_SPEED * 150.0;
+    distance_moved_x = SystemManager:GetUpdateTime() / vt_map.MapMode.NORMAL_SPEED * 170.0;
     if (a_coeff ~= 0.0) then
-        distance_moved_y = (1/a_coeff) * distance_moved_x;
+        distance_moved_y = a_coeff * distance_moved_x;
     end
-    
+
+    -- Make the speed the same whatever the angle between the character and the enemy is.
+    -- We deal only with a coefficients > 1.0 for simplification purpose.
+    if (a_coeff > 1.0 and distance_moved_x ~= 0.0 and distance_moved_y ~= 0.0) then
+        distance_moved_x = distance_moved_x * (distance_moved_x / distance_moved_y);
+        if (a_coeff ~= 0.0) then
+            distance_moved_y = a_coeff * distance_moved_x;
+        end
+        --print ("new_ratio: ", a_coeff, distance_moved_x / distance_moved_y)
+    end
+
     -- Start to run towards the enemy
     if (attack_step == 0) then
         character:ChangeSpriteAnimation("jump_forward")
