18233e310daf64e592bf98039a6d4ca2704cfc8f
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-01-20 19:51:25 +0100

    Added new props.
    Also changed the way certain props graphics were fetched, to void loading
    a whole tileset for a single image.

--- img/sprites/map/objects/jar1.lua
+++ img/sprites/map/objects/jar1.lua
@@ -4,14 +4,14 @@
 animation = {
 
     -- The file to load the frames from
-    image_filename = "img/sprites/map/objects/plate_pile1.png",
+    image_filename = "img/sprites/map/objects/jar1.png",
     -- The number of rows and columns of images, will be used to compute
     -- the images width and height, and also the frames number (row x col)
     rows = 1,
     columns = 1,
     -- set the image dimensions on the map (in pixels)
-    frame_width = 32,
-    frame_height = 32,
+    frame_width = 20,
+    frame_height = 27,
     -- The frames duration in milliseconds
     frames = {
         [0] = { id = 0, duration = 0 } -- 0 means forever
--- img/sprites/map/objects/vase4.lua
+++ img/sprites/map/objects/vase4.lua
@@ -4,13 +4,13 @@
 animation = {
 
     -- The file to load the frames from
-    image_filename = "img/sprites/map/objects/plate_pile1.png",
+    image_filename = "img/sprites/map/objects/vase4.png",
     -- The number of rows and columns of images, will be used to compute
     -- the images width and height, and also the frames number (row x col)
     rows = 1,
     columns = 1,
     -- set the image dimensions on the map (in pixels)
-    frame_width = 32,
+    frame_width = 19,
     frame_height = 32,
     -- The frames duration in milliseconds
     frames = {
