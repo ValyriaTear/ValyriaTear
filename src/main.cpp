54ff947d7c2c8a4ec07270e2b9733901fb16ad71
Yohann Ferreira <yohann.ferreira@orange.fr>
2014-01-29 01:14:30 +0100

    Updated the copyright headers for 2014.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //            Copyright (C) 2004-2011 by The Allacrost Project
-//            Copyright (C) 2012-2013 by Bertram (Valyria Tear)
+//            Copyright (C) 2012-2014 by Bertram (Valyria Tear)
 //                         All Rights Reserved
 //
 // This code is licensed under the GNU GPL version 2. It is free software
