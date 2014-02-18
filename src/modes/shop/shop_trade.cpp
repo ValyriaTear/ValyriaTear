24035972b4f73339ec39f946e7eb64cb76d3e7dc
Yohann Ferreira <Bertram25@users.noreply.github.com>
2014-02-18 16:47:20 +0100

    Fixed displaying key items twice in the trade mode.
    This was due to yet again a bad indexing in the item category handling.

--- src/modes/shop/shop_trade.cpp
+++ src/modes/shop/shop_trade.cpp
@@ -179,8 +179,7 @@ void TradeInterface::Reinitialize()
     }
 
     // Holds the index to the _object_data vector where the container for a specific object type is located
-    // The + 1 is set to reserve space for key items.
-    std::vector<uint32> type_index(GLOBAL_OBJECT_TOTAL + 1, 0);
+    std::vector<uint32> type_index(GLOBAL_OBJECT_TOTAL, 0);
     // Used to set the appropriate data in the type_index vector
     uint32 next_index = 0;
     // Used to do a bit-by-bit analysis of the deal_types variable
@@ -232,10 +231,6 @@ void TradeInterface::Reinitialize()
             IF_PRINT_WARNING(SHOP_DEBUG) << "added object of unknown type: " << obj->GetObject()->GetObjectType() << std::endl;
             break;
         }
-
-        // Also test whether this is a key item.
-        if (obj->GetObject()->IsKeyItem())
-             object_data[type_index[7]].push_back(obj);
 
         // If there is an "All Wares" category, make sure the object gets added there as well
         if(_number_categories > 1) {
