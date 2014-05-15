17b85e02804a29c6fb0e3036d4b74308997ebb56
Yohann Ferreira <Bertram25@users.noreply.github.com>
2014-05-15 17:14:01 +0200

    Added a few props and basically finished the enigma on the ground floor map.

--- dat/maps/mt_elbrus/mt_elbrus_shrine4_script.lua
+++ dat/maps/mt_elbrus/mt_elbrus_shrine4_script.lua
@@ -54,6 +54,8 @@ function Load(m)
     -- Add a mediumly dark overlay
     Map:GetEffectSupervisor():EnableAmbientOverlay("img/ambient/dark.png", 0.0, 0.0, false);
 
+    -- Preload sounds
+    AudioManager:LoadSound("snd/opening_sword_unsheathe.wav", Map);
 end
 
 -- the map update function handles checks done on each game tick.
@@ -79,6 +81,26 @@ function _CreateCharacters()
     Map:AddGroundObject(orlinn);
 end
 
+-- Arrays of spikes used to block the way
+local spike_square1 = {};
+local spike_square2 = {};
+local spike_square3 = {};
+local spike_square4 = {};
+local spike_square5 = {};
+local spike_square6 = {};
+
+-- state of spike squares
+local spike1_up = true;
+local spike2_up = true;
+local spike3_up = true;
+local spike4_up = true;
+local spike5_up = true;
+local spike6_up = true;
+
+local spike_trigger1 = nil;
+local spike_trigger2 = nil;
+local spike_trigger3 = nil;
+
 function _CreateObjects()
     local object = {}
     local npc = {}
@@ -87,6 +109,144 @@ function _CreateObjects()
     local event = {}
 
     _add_flame(61.5, 29);
+
+    _add_bubble(39, 34);
+    _add_bubble(45, 25);
+    _add_bubble(15, 27);
+    _add_bubble(57, 13);
+    _add_bubble(18, 15);
+    _add_bubble(5, 37);
+    _add_bubble(51, 38);
+    _add_bubble(54, 22);
+
+    _add_flame_pot(26, 24);
+    _add_flame_pot(26, 20);
+    _add_flame_pot(26, 16);
+    _add_flame_pot(29, 14.3);
+    _add_flame_pot(33, 14.3);
+
+    _add_flame_pot(42, 24);
+    _add_flame_pot(42, 20);
+    _add_flame_pot(42, 16);
+
+    -- Creates the spikes
+    local spike_objects1 = {
+        -- upper left border
+        { 29, 18 },
+        { 31, 18 },
+        { 29, 20 },
+        { 31, 20 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects1) do
+        spike_square1[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square1[my_index]);
+    end
+
+    local spike_objects2 = {
+        -- upper left border
+        { 33, 18 },
+        { 35, 18 },
+        { 33, 20 },
+        { 35, 20 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects2) do
+        spike_square2[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square2[my_index]);
+    end
+
+    local spike_objects3 = {
+        -- upper left border
+        { 37, 18 },
+        { 39, 18 },
+        { 37, 20 },
+        { 39, 20 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects3) do
+        spike_square3[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square3[my_index]);
+    end
+
+    local spike_objects4 = {
+        -- upper left border
+        { 29, 22 },
+        { 31, 22 },
+        { 29, 24 },
+        { 31, 24 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects4) do
+        spike_square4[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square4[my_index]);
+    end
+
+    local spike_objects5 = {
+        -- upper left border
+        { 33, 22 },
+        { 35, 22 },
+        { 33, 24 },
+        { 35, 24 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects5) do
+        spike_square5[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square5[my_index]);
+    end
+
+    local spike_objects6 = {
+        -- upper left border
+        { 37, 22 },
+        { 39, 22 },
+        { 37, 24 },
+        { 39, 24 },
+    }
+
+    -- Loads the spikes according to the array
+    for my_index, my_array in pairs(spike_objects6) do
+        spike_square6[my_index] = CreateObject(Map, "Spikes1", my_array[1], my_array[2]);
+        Map:AddGroundObject(spike_square6[my_index]);
+    end
+
+    -- 3 triggers permitting to play a bit...
+    spike_trigger1 = vt_map.TriggerObject("",
+                             "img/sprites/map/triggers/stone_trigger1_off.lua",
+                             "img/sprites/map/triggers/stone_trigger1_on.lua",
+                             "", "Push trigger 1");
+    spike_trigger1:SetObjectID(Map.object_supervisor:GenerateObjectID());
+    spike_trigger1:SetPosition(30, 26);
+    Map:AddFlatGroundObject(spike_trigger1);
+
+    spike_trigger2 = vt_map.TriggerObject("",
+                             "img/sprites/map/triggers/stone_trigger1_off.lua",
+                             "img/sprites/map/triggers/stone_trigger1_on.lua",
+                             "", "Push trigger 2");
+    spike_trigger2:SetObjectID(Map.object_supervisor:GenerateObjectID());
+    spike_trigger2:SetPosition(34, 26);
+    Map:AddFlatGroundObject(spike_trigger2);
+
+    spike_trigger3 = vt_map.TriggerObject("",
+                             "img/sprites/map/triggers/stone_trigger1_off.lua",
+                             "img/sprites/map/triggers/stone_trigger1_on.lua",
+                             "", "Push trigger 3");
+    spike_trigger3:SetObjectID(Map.object_supervisor:GenerateObjectID());
+    spike_trigger3:SetPosition(38, 26);
+    Map:AddFlatGroundObject(spike_trigger3);
+
+    -- Make sure the spike square states are initialized
+    spike1_up = true;
+    spike2_up = true;
+    spike3_up = true;
+    spike4_up = true;
+    spike5_up = true;
+    spike6_up = true;
+
 end
 
 function _add_flame(x, y)
@@ -96,6 +256,31 @@ function _add_flame(x, y)
     if (object ~= nil) then Map:AddAmbientSoundObject(object) end;
 
     object = CreateObject(Map, "Flame1", x, y);
+    Map:AddGroundObject(object);
+
+    Map:AddHalo("img/misc/lights/torch_light_mask2.lua", x, y + 3.0,
+        vt_video.Color(0.85, 0.32, 0.0, 0.6));
+    Map:AddHalo("img/misc/lights/sun_flare_light_main.lua", x, y + 2.0,
+        vt_video.Color(0.99, 1.0, 0.27, 0.1));
+end
+
+function _add_bubble(x, y)
+    local object = CreateObject(Map, "Bubble", x, y);
+    object:RandomizeCurrentAnimationFrame();
+    Map:AddGroundObject(object);
+    object = vt_map.ParticleObject("dat/effects/particles/bubble_steam.lua", x, y);
+    object:SetObjectID(Map.object_supervisor:GenerateObjectID());
+    Map:AddGroundObject(object);
+end
+
+function _add_flame_pot(x, y)
+    local object = vt_map.SoundObject("snd/campfire.ogg", x, y, 5.0);
+    if (object ~= nil) then Map:AddAmbientSoundObject(object) end;
+    object = vt_map.SoundObject("snd/campfire.ogg", x + 18.0, y, 5.0);
+    if (object ~= nil) then Map:AddAmbientSoundObject(object) end;
+
+    object = CreateObject(Map, "Flame Pot1", x, y);
+    object:RandomizeCurrentAnimationFrame();
     Map:AddGroundObject(object);
 
     Map:AddHalo("img/misc/lights/torch_light_mask2.lua", x, y + 3.0,
@@ -114,6 +299,12 @@ function _CreateEvents()
                                        "dat/maps/mt_elbrus/mt_elbrus_shrine2_script.lua", "from_shrine_enigma_room");
     EventManager:RegisterEvent(event);
 
+    event = vt_map.ScriptedEvent("Push trigger 1", "push_trigger_1", "");
+    EventManager:RegisterEvent(event);
+    event = vt_map.ScriptedEvent("Push trigger 2", "push_trigger_2", "");
+    EventManager:RegisterEvent(event);
+    event = vt_map.ScriptedEvent("Push trigger 3", "push_trigger_3", "");
+    EventManager:RegisterEvent(event);
 end
 
 -- zones
@@ -138,8 +329,99 @@ function _CheckZones()
 
 end
 
+-- Updates the spikes states
+function _UpdateSpikeState()
+    for my_index, my_object in pairs(spike_square1) do
+        if (spike1_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+
+    for my_index, my_object in pairs(spike_square2) do
+        if (spike2_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+
+    for my_index, my_object in pairs(spike_square3) do
+        if (spike3_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+
+    for my_index, my_object in pairs(spike_square4) do
+        if (spike4_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+
+    for my_index, my_object in pairs(spike_square5) do
+        if (spike5_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+
+    for my_index, my_object in pairs(spike_square6) do
+        if (spike6_up == true) then
+            my_object:SetVisible(true);
+            my_object:SetCollisionMask(vt_map.MapMode.ALL_COLLISION);
+        else
+            my_object:SetVisible(false);
+            my_object:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+        end
+    end
+    AudioManager:PlaySound("snd/opening_sword_unsheathe.wav");
+end
+
 -- Map Custom functions
 -- Used through scripted events
 map_functions = {
 
+    push_trigger_1 = function()
+        spike_trigger2:SetState(false);
+        spike_trigger3:SetState(false);
+
+        spike1_up = not spike1_up;
+        spike3_up = not spike3_up;
+        spike5_up = not spike5_up;
+        _UpdateSpikeState();
+    end,
+
+    push_trigger_2 = function()
+        spike_trigger1:SetState(false);
+        spike_trigger3:SetState(false);
+
+        spike2_up = not spike2_up;
+        spike4_up = not spike4_up;
+        _UpdateSpikeState();
+    end,
+
+    push_trigger_3 = function()
+        spike_trigger1:SetState(false);
+        spike_trigger2:SetState(false);
+
+        spike3_up = not spike3_up;
+        spike4_up = not spike4_up;
+        _UpdateSpikeState();
+    end,
 }
