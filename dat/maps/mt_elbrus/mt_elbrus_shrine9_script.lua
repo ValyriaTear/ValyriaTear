80d3bd478aea1377b87c63a770caa3916729d2f3
Yohann Ferreira <Bertram25@users.noreply.github.com>
2014-06-26 16:23:32 +0200

    Added big stairs base script.

---
+++ dat/maps/mt_elbrus/mt_elbrus_shrine9_script.lua
@@ -0,0 +1,255 @@
+-- Set the namespace according to the map name.
+local ns = {};
+setmetatable(ns, {__index = _G});
+mt_elbrus_shrine9_script = ns;
+setfenv(1, ns);
+
+-- The map name, subname and location image
+map_name = "Mt. Elbrus Shrine"
+map_image_filename = "img/menus/locations/mountain_shrine.png"
+map_subname = ""
+
+-- The music file used as default background music on this map.
+-- Other musics will have to handled through scripting.
+music_filename = "mus/mountain_shrine.ogg"
+
+-- c++ objects instances
+local Map = {};
+local ObjectManager = {};
+local DialogueManager = {};
+local EventManager = {};
+local Effects = {};
+
+-- the main character handler
+local hero = {};
+
+local bronann = nil;
+local kalya = nil;
+local orlinn = nil;
+
+-- Name of the main sprite. Used to reload the good one at the end of the first forest entrance event.
+local main_sprite_name = "";
+
+-- the main map loading code
+function Load(m)
+
+    Map = m;
+    ObjectManager = Map.object_supervisor;
+    DialogueManager = Map.dialogue_supervisor;
+    EventManager = Map.event_supervisor;
+    Effects = Map:GetEffectSupervisor();
+    Map.unlimited_stamina = false;
+
+    _CreateCharacters();
+    _CreateObjects();
+    _CreateEnemies();
+
+    -- Set the camera focus on hero
+    Map:SetCamera(hero);
+    -- This is a dungeon map, we'll use the front battle member sprite as default sprite.
+    Map.object_supervisor:SetPartyMemberVisibleSprite(hero);
+
+    _CreateEvents();
+    _CreateZones();
+
+    -- Add a mediumly dark overlay
+    Effects:EnableAmbientOverlay("img/ambient/dark.png", 0.0, 0.0, false);
+    -- Add the background and foreground animations
+    Map:GetScriptSupervisor():AddScript("dat/maps/layna_forest/layna_forest_caves_background_anim.lua");
+end
+
+-- the map update function handles checks done on each game tick.
+function Update()
+    -- Check whether the character is in one of the zones
+    _CheckZones();
+
+end
+
+-- Character creation
+function _CreateCharacters()
+    -- Default hero and position
+    hero = CreateSprite(Map, "Bronann", 22, 45);
+    hero:SetDirection(vt_map.MapMode.NORTH);
+    hero:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED);
+
+    -- Load previous save point data
+    local x_position = GlobalManager:GetSaveLocationX();
+    local y_position = GlobalManager:GetSaveLocationY();
+    if (x_position ~= 0 and y_position ~= 0) then
+        -- Use the save point position, and clear the save position data for next maps
+        GlobalManager:UnsetSaveLocation();
+        -- Make the character look at us in that case
+        hero:SetDirection(vt_map.MapMode.SOUTH);
+        hero:SetPosition(x_position, y_position);
+    elseif (GlobalManager:GetPreviousLocation() == "from_shrine_main_room") then
+        hero:SetDirection(vt_map.MapMode.NORTH);
+        hero:SetPosition(22.0, 45.0);
+    elseif (GlobalManager:GetPreviousLocation() == "from_shrine_trap_room") then
+        hero:SetDirection(vt_map.MapMode.NORTH);
+        hero:SetPosition(51.0, 45.0);
+    end
+
+    Map:AddGroundObject(hero);
+
+    -- Create other characters
+    bronann = CreateSprite(Map, "Bronann", 0, 0);
+    bronann:SetDirection(vt_map.MapMode.NORTH);
+    bronann:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED);
+    bronann:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+    bronann:SetVisible(false);
+    Map:AddGroundObject(bronann);
+
+    kalya = CreateSprite(Map, "Kalya", 0, 0);
+    kalya:SetDirection(vt_map.MapMode.NORTH);
+    kalya:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED);
+    kalya:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+    kalya:SetVisible(false);
+    Map:AddGroundObject(kalya);
+
+    orlinn = CreateSprite(Map, "Orlinn", 0, 0);
+    orlinn:SetDirection(vt_map.MapMode.NORTH);
+    orlinn:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED);
+    orlinn:SetCollisionMask(vt_map.MapMode.NO_COLLISION);
+    orlinn:SetVisible(false);
+    Map:AddGroundObject(orlinn);
+end
+
+function _CreateObjects()
+    local object = {};
+    local npc = {};
+    local event = {}
+
+    Map:AddHalo("img/misc/lights/right_ray_light.lua", 26, 18,
+            vt_video.Color(1.0, 1.0, 1.0, 0.8));
+    Map:AddHalo("img/misc/lights/right_ray_light.lua", 53, 44,
+            vt_video.Color(1.0, 1.0, 1.0, 0.8));
+
+    Map:AddSavePoint(8, 42);
+
+    -- Load the spring heal effect.
+    heal_effect = vt_map.ParticleObject("dat/effects/particles/heal_particle.lua", 0, 0);
+    heal_effect:SetObjectID(Map.object_supervisor:GenerateObjectID());
+    heal_effect:Stop(); -- Don't run it until the character heals itself
+    Map:AddGroundObject(heal_effect);
+
+    object = CreateObject(Map, "Layna Statue", 6, 35);
+    object:SetEventWhenTalking("Heal dialogue");
+    Map:AddGroundObject(object);
+
+    dialogue = vt_map.SpriteDialogue();
+    text = vt_system.Translate("Your party feels better...");
+    dialogue:AddLineEvent(text, 0, "Heal event", ""); -- 0 means no portrait and no name
+    DialogueManager:AddDialogue(dialogue);
+    event = vt_map.DialogueEvent("Heal dialogue", dialogue);
+    EventManager:RegisterEvent(event);
+
+    event = vt_map.ScriptedEvent("Heal event", "heal_party", "heal_done");
+    EventManager:RegisterEvent(event);
+end
+
+-- Sets common battle environment settings for enemy sprites
+function _SetBattleEnvironment(enemy)
+    enemy:SetBattleMusicTheme("mus/heroism-OGA-Edward-J-Blakeley.ogg");
+    enemy:SetBattleBackground("img/backdrops/battle/mountain_shrine.png");
+    enemy:AddBattleScript("dat/battles/mountain_shrine_battle_anim.lua");
+end
+
+function _CreateEnemies()
+    local enemy = {};
+    local roam_zone = {};
+
+    -- Hint: left, right, top, bottom
+    roam_zone = vt_map.EnemyZone(26, 37, 33, 45);
+
+    enemy = CreateEnemySprite(Map, "Skeleton");
+    _SetBattleEnvironment(enemy);
+    enemy:NewEnemyParty();
+    enemy:AddEnemy(19);
+    enemy:AddEnemy(19);
+    enemy:AddEnemy(19);
+    roam_zone:AddEnemy(enemy, Map, 1);
+    Map:AddZone(roam_zone);
+end
+
+-- Creates all events and sets up the entire event sequence chain
+function _CreateEvents()
+    local event = {};
+    local dialogue = {};
+    local text = {};
+
+    event = vt_map.MapTransitionEvent("to mountain shrine entrance", "dat/maps/mt_elbrus/mt_elbrus_shrine2_map.lua",
+                                       "dat/maps/mt_elbrus/mt_elbrus_shrine2_script.lua", "from_shrine_stairs1");
+    EventManager:RegisterEvent(event);
+    event = vt_map.MapTransitionEvent("to mountain shrine trap room", "dat/maps/mt_elbrus/mt_elbrus_shrine3_map.lua",
+                                       "dat/maps/mt_elbrus/mt_elbrus_shrine3_script.lua", "from_shrine_stairs1");
+    EventManager:RegisterEvent(event);
+
+end
+
+-- zones
+local to_shrine_entrance_zone = nil
+local to_shrine_trap_zone = nil
+local falling_event_zone = nil
+
+-- Create the different map zones triggering events
+function _CreateZones()
+    -- N.B.: left, right, top, bottom
+    to_shrine_entrance_zone = vt_map.CameraZone(20, 24, 46, 48);
+    Map:AddZone(to_shrine_entrance_zone);
+
+    to_shrine_trap_zone = vt_map.CameraZone(50, 52, 46, 48);
+    Map:AddZone(to_shrine_trap_zone);
+
+    falling_event_zone = vt_map.CameraZone(19, 25, 11, 19);
+    Map:AddZone(falling_event_zone);
+end
+
+-- Check whether the active camera has entered a zone. To be called within Update()
+function _CheckZones()
+    if (to_shrine_entrance_zone:IsCameraEntering() == true) then
+        hero:SetMoving(false);
+        EventManager:StartEvent("to mountain shrine entrance");
+    elseif (to_shrine_trap_zone:IsCameraEntering() == true) then
+        hero:SetMoving(false);
+        EventManager:StartEvent("to mountain shrine trap room");
+    end
+end
+
+-- Map Custom functions
+-- Used through scripted events
+
+-- Effect time used when applying the heal light effect
+local heal_effect_time = 0;
+local heal_color = vt_video.Color(0.0, 0.0, 1.0, 1.0);
+
+map_functions = {
+
+    heal_party = function()
+        hero:SetMoving(false);
+        -- Should be sufficient to heal anybody
+        GlobalManager:GetActiveParty():AddHitPoints(20000);
+        GlobalManager:GetActiveParty():AddSkillPoints(20000);
+        Map:SetStamina(10000);
+        AudioManager:PlaySound("snd/heal_spell.wav");
+        heal_effect:SetPosition(hero:GetXPosition(), hero:GetYPosition());
+        heal_effect:Start();
+        heal_effect_time = 0;
+    end,
+
+    heal_done = function()
+        heal_effect_time = heal_effect_time + SystemManager:GetUpdateTime();
+
+        if (heal_effect_time < 300.0) then
+            heal_color:SetAlpha(heal_effect_time / 300.0 / 3.0);
+            Map:GetEffectSupervisor():EnableLightingOverlay(heal_color);
+            return false;
+        end
+
+        if (heal_effect_time < 1000.0) then
+            heal_color:SetAlpha(((1000.0 - heal_effect_time) / 700.0) / 3.0);
+            Map:GetEffectSupervisor():EnableLightingOverlay(heal_color);
+            return false;
+        end
+        return true;
+    end,
+}
