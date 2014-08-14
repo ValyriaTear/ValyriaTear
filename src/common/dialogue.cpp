44e8b39617c2a04a49d8d30034d305f09f439d2b
Yohann Ferreira <Bertram25@users.noreply.github.com>
2014-08-14 12:18:21 +0200

    Tweaked a bit the location of dialogue options.

--- src/common/dialogue.cpp
+++ src/common/dialogue.cpp
@@ -226,7 +226,7 @@ void CommonDialogueWindow::SetPosition(float pos_x, float pos_y)
     _pos_y = pos_y;
 
     _display_textbox.SetPosition(_pos_x + 80.0f, _pos_y - 90.0f);
-    _display_optionbox.SetPosition(_pos_x - 260.0f, _pos_y - 112.0f);
+    _display_optionbox.SetPosition(_pos_x - 220.0f, _pos_y - 112.0f);
 }
 
 
