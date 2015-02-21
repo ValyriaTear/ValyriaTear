dd20a0874329608b34ac1719d730554fc0007f0a
Yohann Ferreira <yohann.ferreira@orange.fr>
2015-02-21 12:34:39 +0100

    I split the video common enum from the big video header but forgot to commit the new file.

---
+++ src/engine/video/video_utils.h
@@ -0,0 +1,80 @@
+///////////////////////////////////////////////////////////////////////////////
+//            Copyright (C) 2004-2011 by The Allacrost Project
+//            Copyright (C) 2012-2015 by Bertram (Valyria Tear)
+//                         All Rights Reserved
+//
+// This code is licensed under the GNU GPL version 2. It is free software
+// and you may modify it and/or redistribute it under the terms of this license.
+// See http://www.gnu.org/copyleft/gpl.html for details.
+///////////////////////////////////////////////////////////////////////////////
+
+/** ****************************************************************************
+*** \file    video_utils.h
+*** \author  Raj Sharma, roos@allacrost.org
+*** \author  Daniel Steuernol, steu@allacrost.org
+*** \author  Yohann Ferreira, yohann ferreira orange fr
+*** \brief   Header file for video utils constants.
+***
+*** Provides the default constants used for the Video component.
+***
+*** \note This code uses the OpenGL library for graphics rendering.
+*** \note This code uses the SDL_Image library to load images.
+*** \note This code uses the SDL_ttf 2.0 library for font rendering.
+*** ***************************************************************************/
+
+#ifndef __VIDEO_UTILS_HEADER__
+#define __VIDEO_UTILS_HEADER__
+
+//! \brief All calls to the video engine are wrapped in this namespace.
+namespace vt_video
+{
+//! \brief The standard screen resolution
+const float VIDEO_STANDARD_RES_WIDTH  = 1024.0f;
+const float VIDEO_STANDARD_RES_HEIGHT = 768.0f;
+
+//! \brief The number of FPS samples to retain across frames
+const uint32 FPS_SAMPLES = 250;
+
+//! \brief Draw flags to control x and y alignment, flipping, and texture blending.
+enum VIDEO_DRAW_FLAGS {
+    VIDEO_DRAW_FLAGS_INVALID = -1,
+
+    //! X draw alignment flags
+    //@{
+    VIDEO_X_LEFT = 1,
+    VIDEO_X_CENTER = 2,
+    VIDEO_X_RIGHT = 3,
+    //@}
+
+    //! Y draw alignment flags
+    //@{
+    VIDEO_Y_TOP = 4,
+    VIDEO_Y_CENTER = 5,
+    VIDEO_Y_BOTTOM = 6,
+    //@}
+
+    //! X flip flags
+    //@{
+    VIDEO_X_FLIP = 7,
+    VIDEO_X_NOFLIP = 8,
+    //@}
+
+    //! Y flip flags
+    //@{
+    VIDEO_Y_FLIP = 9,
+    VIDEO_Y_NOFLIP = 10,
+    //@}
+
+    //! Texture blending flags
+    //@{
+    VIDEO_NO_BLEND = 11,
+    VIDEO_BLEND = 12,
+    VIDEO_BLEND_ADD = 13,
+    //@}
+
+    VIDEO_DRAW_FLAGS_TOTAL = 14
+};
+
+} // namespace vt_video
+
+#endif // __VIDEO_UTILS_HEADER__
