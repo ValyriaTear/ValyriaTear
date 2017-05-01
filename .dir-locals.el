b8cbc7bfcc552e24c2991e93f50cc02affb06bca
Nicola Spanti (RyDroid) <dev@nicola-spanti.info>
2017-05-01 17:43:35 +0200

    Minor additions for users of GNU Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;; Copying and distribution of this file, with or without modification,
+;; are permitted in any medium without royalty provided this notice is
+;; preserved.  This file is offered as-is, without any warranty.
+;; Names of contributors must not be used to endorse or promote products
+;; derived from this file without specific prior written permission.
+
+;; Usefull for GNU Emacs
+((nil .
+     ((indent-tabs-mode . nil)))
+ (makefile-mode . ((indent-tabs-mode . t))))
