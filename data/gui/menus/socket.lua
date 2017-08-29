9ce77be2fa6f3bcbd38d96e2a069f4ed407cc09b
Yohann Ferreira <yohann.ferreira@orange.fr>
2017-08-30 00:42:37 +0200

    Added animation files for icons in the skill graph
    Refs #389

--- data/gui/menus/socket.lua
+++ data/gui/menus/socket.lua
@@ -4,23 +4,16 @@
 animation = {
 
     -- The file to load the frames from
-    image_filename = "data/gui/menus/rotating_crystal_grey.png",
+    image_filename = "data/gui/menus/socket.png",
     -- The number of rows and columns of images, will be used to compute
     -- the images width and height, and also the frames number (row x col)
     rows = 1,
-    columns = 8,
+    columns = 1,
     -- set the image dimensions (in pixels)
     frame_width = 16.0,
     frame_height = 16.0,
     -- The frames duration in milliseconds
     frames = {
-        [0] = { id = 0, duration = 120 },
-        [1] = { id = 1, duration = 120 },
-        [2] = { id = 2, duration = 120 },
-        [3] = { id = 3, duration = 120 },
-        [4] = { id = 4, duration = 120 },
-        [5] = { id = 5, duration = 120 },
-        [6] = { id = 6, duration = 120 },
-        [7] = { id = 7, duration = 120 }
+        [0] = { id = 0, duration = 0 } -- forever
     }
 }
