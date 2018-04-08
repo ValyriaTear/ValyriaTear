8b04c93e4d3d42d7b76a481d1b205e422491f08e
Yohann Ferreira <yohann.ferreira@orange.fr>
2018-04-09 01:15:36 +0200

    Added Oil Lamp map object and night effect in the well map.
    Refs #604

--- data/story/layna_village/layna_village_well_underground_script.lua
+++ data/story/layna_village/layna_village_well_underground_script.lua
@@ -15,6 +15,7 @@ music_filename = "data/music/shrine-OGA-yd.ogg"
 
 -- c++ objects instances
 local Map = nil
+local Script = nil
 local EventManager = nil
 
 -- Characters handler
@@ -25,42 +26,53 @@ local olivia = nil
 function Load(m)
 
     Map = m;
-    EventManager = Map:GetEventSupervisor();
+    Script = Map:GetScriptSupervisor()
+    EventManager = Map:GetEventSupervisor()
 
-    _CreateCharacters();
-    _CreateObjects();
+    -- Put last to get a proper night effect
+    Script:AddScript("data/story/common/at_night.lua")
+
+
+    _CreateCharacters()
+    _CreateObjects()
 
     -- Set the camera focus on bronann
-    Map:SetCamera(bronann);
+    Map:SetCamera(bronann)
 
-    _CreateEvents();
-    _CreateZones();
+    _CreateEvents()
+    _CreateZones()
 end
 
 -- the map update function handles checks done on each game tick.
 function Update()
     -- Check whether the character is in one of the zones
-    _CheckZones();
+    _CheckZones()
+
+    _UpdateLampLocation()
 end
 
 -- Character creation
 function _CreateCharacters()
-    bronann = CreateSprite(Map, "Bronann", 18, 2, vt_map.MapMode.GROUND_OBJECT);
-    bronann:SetDirection(vt_map.MapMode.NORTH);
-    bronann:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED);
+    bronann = CreateSprite(Map, "Bronann", 18, 2, vt_map.MapMode.GROUND_OBJECT)
+    bronann:SetDirection(vt_map.MapMode.NORTH)
+    bronann:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED)
 
     olivia = CreateNPCSprite(Map, "Girl1", vt_system.Translate("Olivia"), 18, 2, vt_map.MapMode.GROUND_OBJECT)
     olivia:SetDirection(vt_map.MapMode.SOUTH)
     olivia:SetMovementSpeed(vt_map.MapMode.NORMAL_SPEED)
 end
 
-local chicken1 = nil
+local oil_lamp = nil
 
 function _CreateObjects()
     local object = nil
 
-    --CreateObject(Map, "Bed1", 42, 27, vt_map.MapMode.GROUND_OBJECT);
+    oil_lamp = CreateObject(Map, "Oil Lamp", 15, 14, vt_map.MapMode.GROUND_OBJECT);
 
+end
+
+function _UpdateLampLocation()
+    -- TODO: Sets the lamp location depending on the character's location
 end
 
 -- Creates all events and sets up the entire event sequence chain
@@ -71,7 +83,7 @@ function _CreateEvents()
 
     -- Triggered events
     vt_map.MapTransitionEvent.Create("exit floor", "data/story/layna_village/layna_village_center_map.lua",
-                                     "data/story/layna_village/layna_village_center_script.lua", "from_well_undergrounds");
+                                     "data/story/layna_village/layna_village_center_script.lua", "from_well_undergrounds")
 
     -- Generic events
     vt_map.LookAtSpriteEvent.Create("Olivia looks at Bronann", olivia, bronann)
@@ -91,14 +103,14 @@ local room_exit_zone = nil
 -- Create the different map zones triggering events
 function _CreateZones()
     -- N.B.: left, right, top, bottom
-    room_exit_zone = vt_map.CameraZone.Create(16, 18, 0, 1);
+    room_exit_zone = vt_map.CameraZone.Create(16, 18, 0, 1)
 end
 
 -- Check whether the active camera has entered a zone. To be called within Update()
 function _CheckZones()
     if (room_exit_zone:IsCameraEntering() == true) then
         bronann:SetMoving(false);
-        EventManager:StartEvent("exit floor");
+        EventManager:StartEvent("exit floor")
     end
 end
 
