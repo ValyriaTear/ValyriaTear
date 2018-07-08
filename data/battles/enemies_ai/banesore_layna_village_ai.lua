0c7210cf1c02297a097e03c0ce66778ded2492c7
Yohann Ferreira <yohann.ferreira@orange.fr>
2018-07-09 01:36:58 +0200

    Improved Banesore AI to avoid killing Bronann too easily.

--- data/battles/enemies_ai/banesore_layna_village_ai.lua
+++ data/battles/enemies_ai/banesore_layna_village_ai.lua
@@ -11,7 +11,7 @@ setfenv(1, ns);
 -- 10120   -- Wave - 7 SP
 
 -- Instance local vars
-local atk_spell_done = false;
+local atk_spell_ratio = 0;
 
 -- The battle_actor parameter is the enemy thinking, useful to exclude itself from other opponents.
 function DecideAction(battle_instance, battle_actor)
@@ -21,34 +21,31 @@ function DecideAction(battle_instance, battle_actor)
     local Banesore = battle_actor;
     local Bronann = Battle:GetCharacterActor(0);
 
-
     -- Test whether Banesore has attacked once with a spell
-    if (atk_spell_done == false) then
-
+    if (Bronann:GetHitPoints() > 80 and atk_spell_ratio > 3) then
         -- Triggers a random spell
         local spell_skill_id = 10100; -- Fire burst
         if (math.random(0, 100) >= 51) then
             spell_skill_id = 10120; -- Wave
         end
         Banesore:SetAction(spell_skill_id, Bronann);
-        atk_spell_done = true;
+        atk_spell_ratio = 0;
         return;
     end
 
-    local banesore_shield_ok = false;
     -- Test the current Shield spell strength
     if (Banesore:GetActiveStatusEffectIntensity(vt_global.GameGlobal.GLOBAL_STATUS_PHYS_DEF)
-            >= vt_global.GameGlobal.GLOBAL_INTENSITY_POS_GREATER) then
+            >= vt_global.GameGlobal.GLOBAL_INTENSITY_POS_GREATER
+            or Bronann:GetHitPoints() < 60) then
 
         -- Default attack on the hero
-        Banesore:SetAction(1006, Bronann);
+        Banesore:SetAction(1006, Bronann)
     else
         -- Triggers a Shield spell
-        Banesore:SetAction(10001, Banesore);
+        Banesore:SetAction(10001, Banesore)
     end
 
-    -- Since no atk spell has been done, we set this value back
-    -- for the next action.
-    atk_spell_done = false;
+    -- Since no atk spell has been done, increase this value for the next action.
+    atk_spell_ratio = atk_spell_ratio + 1
 
 end
