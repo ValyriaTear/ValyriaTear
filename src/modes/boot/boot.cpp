e206f0f25479e35962d49da738573c48077702c3
Yohann Ferreira <yohann.ferreira@orange.fr>
2019-09-09 22:42:11 +0200

    Fixed game and installer version, example and missing dll.
    Refs #554

--- src/modes/boot/boot.cpp
+++ src/modes/boot/boot.cpp
@@ -96,7 +96,7 @@ BootMode::BootMode() :
 
     // Note: Not translated on purpose.
     _version_text.SetStyle(TextStyle("text18"));
-    std::string version_string = "Version 1.0.0 - Development version";
+    std::string version_string = "Version 1.1.0 - Beta 1";
 
     // NOTE: Only leave the " - " part for releases.
     version_string.append(" - ");
