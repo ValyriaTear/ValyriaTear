d6766eec91f102f74669330b115351c424bc68b0
Yohann Ferreira <yohann.ferreira@orange.fr>
2019-09-13 17:58:02 +0200

    Fixed the update-pot bash script to also take map subnames.
    It was well hidden as the first maps subname were also in quests, making it previously unseen.
    
    Refs #643

--- po/create_map_names_translatable_strings.sh
+++ po/create_map_names_translatable_strings.sh
@@ -1,18 +1,20 @@
 # Empties the map names translation files
 echo -n "" > map_names_tr.lua
 
+# Get map name and map subname
 cat map_names_files |
 while read a; do
-
-    # Get map name or map subname
     MAP_NAME=""
-    MAP_NAME=$(cat  $a | grep 'map_name' | cut -d'=' -f2 | sed s/\ \"/\"/)
-    if [ "$MAP_NAME" = "\"\"" -o "$MAP_NAME" = "" ]; then
-        MAP_NAME=$(cat  $a | grep 'map_subname' | cut -d'=' -f2 | sed s/\ \"/\"/)
-    fi
     # Add map name if it's not empty
+    MAP_NAME=$(cat  $a | grep 'map_name' | cut -d'=' -f2 | sed s/\ \"/\"/)
+
     if [[ "$MAP_NAME" != "\"\"" && "$MAP_NAME" != "" ]]; then
         echo "/// tr: Map name" >> map_names_tr.lua
+        echo "Translate("$MAP_NAME")" >> map_names_tr.lua
+    fi
+    MAP_NAME=$(cat  $a | grep 'map_subname' | cut -d'=' -f2 | sed s/\ \"/\"/)
+    if [[ "$MAP_NAME" != "\"\"" && "$MAP_NAME" != "" ]]; then
+        echo "/// tr: Map sub-name" >> map_names_tr.lua
         echo "Translate("$MAP_NAME")" >> map_names_tr.lua
     fi
 done
