34a23b257fa2dc9d68508797af793b4892cd552b
Yohann Ferreira <yohann.ferreira@orange.fr>
2019-09-23 00:41:31 +0200

    Split the translation pot files and their corresponding translations
    Refs #341

--- po/create_map_names_translatable_strings.sh
+++ po/create_map_names_translatable_strings.sh
@@ -1,20 +1,24 @@
 # Empties the map names translation files
-echo -n "" > map_names_tr.lua
+
+input_filename="$1"
+output_filename="$input_filename-tr.lua"
+
+echo -n "" > $output_filename
 
 # Get map name and map subname
-cat map_names_files |
+cat $input_filename |
 while read a; do
     MAP_NAME=""
     # Add map name if it's not empty
     MAP_NAME=$(cat  $a | grep 'map_name' | cut -d'=' -f2 | sed s/\ \"/\"/)
 
     if [[ "$MAP_NAME" != "\"\"" && "$MAP_NAME" != "" ]]; then
-        echo "/// tr: Map name" >> map_names_tr.lua
-        echo "Translate("$MAP_NAME")" >> map_names_tr.lua
+        echo "/// tr: Map name" >> $output_filename
+        echo "Translate("$MAP_NAME")" >> $output_filename
     fi
     MAP_NAME=$(cat  $a | grep 'map_subname' | cut -d'=' -f2 | sed s/\ \"/\"/)
     if [[ "$MAP_NAME" != "\"\"" && "$MAP_NAME" != "" ]]; then
-        echo "/// tr: Map sub-name" >> map_names_tr.lua
-        echo "Translate("$MAP_NAME")" >> map_names_tr.lua
+        echo "/// tr: Map sub-name" >> $output_filename
+        echo "Translate("$MAP_NAME")" >> $output_filename
     fi
 done
